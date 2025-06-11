f5e4cabbbc13092c1067b4a1fea31e91f6327b6c
HUANG XING <your-email@example.com>
2025-06-12 00:44:48 +0700

    Fix deployment configuration

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn main:app -c gunicorn_config.py 
+web: gunicorn main:app -c gunicorn_config.py
--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,4 +1,5 @@
 bind = "0.0.0.0:10000"
-workers = 4
+workers = 2
 threads = 2
-timeout = 120 
+timeout = 120
+worker_class = "gthread" 
